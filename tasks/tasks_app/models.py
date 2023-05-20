2a7ff3146d8c1cb48ba3d5aece93e78612a8db07
sam <sementinchurin@gmail.com>
2023-05-20 16:25:50 +0300

    TextField for content

--- tasks/tasks_app/models.py
+++ tasks/tasks_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField(max_length=500)
+    content = models.TextField(max_length=500)
     date_posted = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE)
